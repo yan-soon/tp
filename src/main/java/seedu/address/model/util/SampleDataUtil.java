f3f1941b72f83daedc835673e370fd10e1b6da3f
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-09-29 15:06:00 +0800

    Refactor SampleDataUtil class to generate sample Modules instead of Persons

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,44 +6,36 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.module.ModularCredits;
+import seedu.address.model.module.ModuleCode;
+import seedu.address.model.module.Module;
 import seedu.address.model.tag.Tag;
 
 /**
  * Contains utility methods for populating {@code AddressBook} with sample data.
  */
 public class SampleDataUtil {
-    public static Person[] getSamplePersons() {
-        return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+    public static Module[] getSampleModules() {
+        return new Module[] {
+            new Module(new ModuleCode("Alex Yeoh"), new ModularCredits("87438807"),
+                       getTagSet("friends")),
+            new Module(new ModuleCode("Bernice Yu"), new ModularCredits("99272758"),
+                       getTagSet("colleagues", "friends")),
+            new Module(new ModuleCode("Charlotte Oliveiro"), new ModularCredits("93210283"),
+                       getTagSet("neighbours")),
+            new Module(new ModuleCode("David Li"), new ModularCredits("91031282"),
+                       getTagSet("family")),
+            new Module(new ModuleCode("Irfan Ibrahim"), new ModularCredits("92492021"),
+                       getTagSet("classmates")),
+            new Module(new ModuleCode("Roy Balakrishnan"), new ModularCredits("92624417"),
+                       getTagSet("colleagues"))
         };
     }
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
         AddressBook sampleAb = new AddressBook();
-        for (Person samplePerson : getSamplePersons()) {
-            sampleAb.addPerson(samplePerson);
+        for (Module sampleModule : getSampleModules()) {
+            sampleAb.addModule(sampleModule);
         }
         return sampleAb;
     }

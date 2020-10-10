8216771334738ee3522e3bd410066f3375eca911
Muhammad Syafiq Bin Abas <syafiqabs7@gmail.com>
2020-10-10 13:07:06 +0800

    changed file paths from addressbook.json to gradpad.json

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setGradPadFilePath(Paths.get("addressbook.json"));
+        userPrefs.setGradPadFilePath(Paths.get("gradpad.json"));
         return userPrefs;
     }
 

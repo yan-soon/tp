efe5d2038a95b46f959ced3ccbf96035adad26c0
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-10-16 18:01:56 +0800

    Create HttpUtil class and add method to make GET requests

---
+++ src/main/java/seedu/address/commons/util/HttpUtil.java
@@ -0,0 +1,40 @@
+package seedu.address.commons.util;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import seedu.address.nusmods.exceptions.NusmodsException;
+
+/**
+ * Utilities for HTTP requests.
+ */
+public class HttpUtil {
+
+    /**
+     * Makes a HTTP GET request to a URL and returns the response as a string.
+     *
+     * @param urlString The URL to send the GET request to.
+     * @return The HTTP response as a string.
+     * @throws NusmodsException an error occurs while making the request.
+     */
+    @SuppressWarnings("checkstyle:AbbreviationAsWordInName")
+    public static String makeGETRequest(String urlString) throws NusmodsException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                                      .timeout(Duration.ofSeconds(3))
+                                      .uri(URI.create(urlString))
+                                      .GET()
+                                      .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException ex) {
+            throw new NusmodsException(ex);
+        }
+    }
+}

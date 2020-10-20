c513e3b45d2042358db5d5233a4f9752a3aa18bf
Soon Xiang, Yan <65216986+y4ns00n@users.noreply.github.com>
2020-10-20 13:18:03 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -6,12 +6,12 @@ import seedu.address.model.Model;
  * Format full help instructions for every command for display.
  */
 public class HelpCommand extends Command {
-    public static final String ADD_COMMAND = "add c/CS1231 cr/4 t/Core : Adds module 'CS1231'" +
-            "with 4 credits and a 'Core' tag to it.";
+    public static final String ADD_COMMAND = "add c/CS1231 cr/4 t/Core : Adds module 'CS1231'"
+            + "with 4 credits and a 'Core' tag to it.";
     public static final String HELP_COMMAND = "help : Displays the help guide.";
     public static final String LIST_COMMAND = "list : Displays list of current modules stored";
-    public static final String EDIT_COMMAND = "edit 1 c/CS1231 cr/4 :" +
-            "Replaces the current module at index 1 with module 'CS1231' with 4 credits.";
+    public static final String EDIT_COMMAND = "edit 1 c/CS1231 cr/4 :"
+            + "Replaces the current module at index 1 with module 'CS1231' with 4 credits.";
     public static final String FIND_COMMAND = "find CS1231 : Searches for the 'CS1231' module.";
     public static final String DELETE_COMMAND = "delete 1 : Deletes the module at index 1.";
     public static final String CHECKMC_COMMAND = "checkmc : Checks the current total credits achieved by all modules.";

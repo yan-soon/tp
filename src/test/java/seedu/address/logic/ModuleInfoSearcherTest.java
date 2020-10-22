7a08264ee097f80b6736870df6cd4b24dd522b7d
shaokiat <shaokiat@gmail.com>
2020-10-22 23:34:00 +0800

    Update test cases for ModuleInfoSearcherTest

--- src/test/java/seedu/address/logic/ModuleInfoSearcherTest.java
+++ src/test/java/seedu/address/logic/ModuleInfoSearcherTest.java
@@ -1,20 +1,52 @@
 package seedu.address.logic;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
-import seedu.address.nusmods.NusmodsDataManager;
 
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.commons.util.JsonUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.nusmods.ModuleInfo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Contains integration tests for {@code ModuleInfoSearcher}.
+ */
 class ModuleInfoSearcherTest {
+
+    private static final String CS1010X = "src/test/data/NusmodsDataManagerTest/CS1010X.json";
+
 
     private ModuleInfoSearcher moduleInfoSearcher = new ModuleInfoSearcher();
 
     @Test
     public void search_empty_module() {
         String emptyModule = "";
-//        assertCommandException(moduleInfoSearcher.searchModule(emptyModule), ModuleInfoSearcher.MESSAGE_EMPTY_SEARCH);
+        assertThrows(CommandException.class, ModuleInfoSearcher.MESSAGE_EMPTY_SEARCH, ()
+            -> moduleInfoSearcher.searchModule(emptyModule));
+    }
+
+    @Test
+    public void search_false_module() {
+        String falseModule = "AA0000";
+        assertThrows(CommandException.class, ModuleInfoSearcher.MESSAGE_FAILED_TO_FIND_MODULE, ()
+            -> moduleInfoSearcher.searchModule(falseModule));
+    }
+
+    @Test
+    public void search_success() throws DataConversionException {
+        ModuleInfo cs1010x = JsonUtil.readJsonFile(Paths.get(CS1010X), ModuleInfo.class).get();
+        String moduleCode = "CS1010X";
+        ModuleInfo actualModuleInfo = null;
+        try {
+            actualModuleInfo = moduleInfoSearcher.searchModule(moduleCode);
+        } catch (CommandException e) {
+            assert false;
+        }
+        assertEquals(actualModuleInfo, cs1010x);
     }
 
 }

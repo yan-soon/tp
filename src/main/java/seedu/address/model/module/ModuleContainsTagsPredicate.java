5d89c38e1984818fcc613c99ed30ea57fb11c364
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-10-26 22:24:28 +0800

    Create ModuleContainsTagsPredicate

---
+++ src/main/java/seedu/address/model/module/ModuleContainsTagsPredicate.java
@@ -0,0 +1,28 @@
+package seedu.address.model.module;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Tests that a {@code Module} contains a {@code Tag} matching any of the tag names given.
+ */
+public class ModuleContainsTagsPredicate implements Predicate<Module> {
+    private final List<String> tagNames;
+
+    /**
+     * Creates a new ModuleContainsTagsPredicate to test whether a module contains any tag that
+     * matches any of the tag names in the specified list.
+     *
+     * @param tagNames list of tag names.
+     */
+    public ModuleContainsTagsPredicate(List<String> tagNames) {
+        this.tagNames = tagNames;
+    }
+
+    @Override
+    public boolean test(Module module) {
+        return tagNames.stream().anyMatch(name ->
+              module.getTags().stream().anyMatch(tag ->
+                     tag.tagName.equals(name)));
+    }
+}

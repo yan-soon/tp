dd90be33b9936369e9eedbfdddfd6246410ecfc8
shaokiat <shaokiat@gmail.com>
2020-10-27 19:12:35 +0800

    Fix checkstyle issue

--- src/test/java/seedu/address/logic/parser/GradPadParserTest.java
+++ src/test/java/seedu/address/logic/parser/GradPadParserTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_MODULE;
 import static seedu.address.testutil.TypicalModuleCodes.CODE_FIRST_MODULE;
 
 import java.util.Arrays;

0ba10ef0317f9f67d4d87001d0b82379ffeeeeed
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-10-27 13:36:09 +0800

    Refactor FindCommandParser to use a compound predicate class

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -30,10 +30,30 @@ public class FindCommandParser implements Parser<FindCommand> {
         }
 
         List<String> keywords = Arrays.asList(trimmedArgs.split("\\s+"));
-        Predicate<Module> moduleCodePredicate = new ModuleCodeContainsKeywordsPredicate(keywords);
-        Predicate<Module> tagsPredicate = new ModuleContainsTagsPredicate(keywords);
 
-        return new FindCommand(moduleCodePredicate.or(tagsPredicate));
+        return new FindCommand(new CompoundFindPredicate(keywords));
     }
 
+    public static class CompoundFindPredicate implements Predicate<Module> {
+        private final List<String> keywords;
+
+        CompoundFindPredicate(List<String> keywords) {
+            this.keywords = keywords;
+        }
+
+        @Override
+        public boolean test(Module module) {
+            Predicate<Module> moduleCodePredicate = new ModuleCodeContainsKeywordsPredicate(keywords);
+            Predicate<Module> tagsPredicate = new ModuleContainsTagsPredicate(keywords);
+            // chain the predicates
+            return moduleCodePredicate.or(tagsPredicate).test(module);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return other == this // short circuit if same object
+                           || (other instanceof CompoundFindPredicate // instanceof handles nulls
+                                       && keywords.equals(((CompoundFindPredicate) other).keywords)); // state check
+        }
+    }
 }
